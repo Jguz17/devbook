b730aedb9354eadf986920cca812bf3c6a8a99a9
jguz17 <johnathanguzman17@gmail.com>
2020-08-12 18:23:39 -0500

    setup api routes

--- server.js
+++ server.js
@@ -11,6 +11,12 @@ const app = express()
 // Api call to server, sending some data over
 app.get('/', (req, res) => res.send('API Running'))
 
+// Define routes
+app.use('/api/users', require('./routes/api/users'))
+app.use('/api/auth', require('./routes/api/auth'))
+app.use('/api/profile', require('./routes/api/profile'))
+app.use('/api/posts', require('./routes/api/posts'))
+
 // assinging port to PORT
 // this is saying that when deployed, it is going to use that port no., else use 5000
 const PORT = process.env.PORT || 5000
