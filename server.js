d54718a5473bd20173d7b696ae2f9230b43b44ae
jguz17 <johnathanguzman17@gmail.com>
2020-08-11 23:22:43 -0500

    setting up db

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
+// importing express
 const express = require('express')
 
+const connectDB = require('./config/db')
+
+connectDB()
+
+// assigning function express() from express to app
 const app = express()
 
+// Api call to server, sending some data over
 app.get('/', (req, res) => res.send('API Running'))
 
+// assinging port to PORT
+// this is saying that when deployed, it is going to use that port no., else use 5000
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
