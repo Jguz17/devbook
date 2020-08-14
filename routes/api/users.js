dafa10061a242ad8e616a9bd9b1a32eda2bdba0d
jguz17 <johnathanguzman17@gmail.com>
2020-08-13 21:42:40 -0500

    jwt

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const router = express.Router()
 const gravatar = require('gravatar')
+const config = require('config')
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 const { check, validationResult } = require('express-validator')
 
 const User = require('../../models/User')
@@ -48,9 +50,18 @@ router.post('/', [
 
         await user.save()
 
-        // Return jsonwebtoken 
-        
-        res.send('User registered')
+        const payload = {
+            user: {
+                id: user.id
+            }
+        }
+
+        jwt.sign(payload, config.get('jwtSecret'),{
+            expiresIn: 360000
+        }, (err, token) => {
+            if(err) throw err;
+            res.json({ token })
+        })
     } catch(err) {
         console.log(err.message)
         res.status(500).send('Server error')
